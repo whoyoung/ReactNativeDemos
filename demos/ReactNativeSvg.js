98b015e8ca08ab7acf912130c879359e5b566b96
杨虎 <yanghu@xiaoshouyi.com>
2018-04-13 16:19:06 +0800

    沿曲线分布的文字

--- demos/ReactNativeSvg.js
+++ demos/ReactNativeSvg.js
@@ -7,24 +7,41 @@ import Svg, {
     G,
     Line,
     Path,
-    Text
+    Text,
+    TSpan,
+    Defs,
+    TextPath
 } from 'react-native-svg';
 
 export default class ReactNativeSvg extends Component {
     render() {
         return <Svg
-            height="60"
-            width="200"
-        >
+        height="300"
+        width="300"
+    >
+        <Defs>
+            <Path
+                id="path"
+                d='M25 10 L138 65 L220 190'
+            />
+        </Defs>
+        <G y="20">
             <Text
+                fill="blue"
+    
+            >
+                <TextPath href="#path" startOffset="10%">
+                    go up and down, then up again
+                    
+                </TextPath>
+            </Text>
+            <Path
+                d='M25 10 L138 65 L220 190'
                 fill="none"
-                stroke="purple"
-                fontSize="20"
-                fontWeight="bold"
-                x="100"
-                y="20"
-                textAnchor="middle"
-            >STROKED TEXT</Text>
-        </Svg>
+                stroke="black"
+                strokeWidth="2"
+            />
+        </G>
+    </Svg>
     }
 }
